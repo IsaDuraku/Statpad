2202492a1c256daea3697a216f200b418823ce77
arlinndi9 <arlinndi9@gmail.com>
2023-10-02 17:27:03 +0200

    create routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
+import uvicorn
 from fastapi import FastAPI
 from app.routers import user_routes
+from app.routers import bet
+from app.routers import telegrafi
 
 app = FastAPI()
 
 app.include_router(user_routes.router, prefix="/api")
+app.include_router(bet.router)
+app.include_router(telegrafi.router)
 
+
+@app.get('/')
+def hello():
+    return {'message': 'hello'}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
