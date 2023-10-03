2d7685ca233f5f0b0a62a9aee954ebc59df8d9cf
Temal Haliti <temal.haliti@makerminds.org>
2023-10-03 13:21:39 +0200

    Minor change on main.py

--- app/main.py
+++ app/main.py
@@ -11,11 +11,6 @@ app.include_router(bet.router)
 app.include_router(telegrafi.router)
 
 
-@app.get('/')
-def hello():
-    return {'message': 'hello'}
-
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
