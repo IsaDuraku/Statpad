a801bce51ddd67fc4aa8aaf012ae1ea4830e0f6a
IsaDuraku <isaduraku3@gmail.com>
2023-10-05 10:49:28 +0200

    GetDatabase info

--- app/routers/livestream.py
+++ app/routers/livestream.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.scrapers.livestream_links import scrape_webpage, insert_links_into_database
 from app.database import SessionLocal
+from app.models.livestream_links import Livestream_links
 
 
 router = APIRouter()
@@ -18,3 +19,9 @@ async def scrape_and_insert(db: Session = Depends(get_db)):
     extracted_matches = scrape_webpage(url)
     insert_links_into_database(extracted_matches, db)
     return extracted_matches
+#ndreqe qeto posht
+@router.get("/get-links-fromDb")
+async def get_matches(db: Session = Depends(get_db)):
+    matches = db.query(Livestream_links).all()
+    return matches
+
