dd198042365a63d36e10c7965a92afd59aa78c3e
arlinndi9 <arlinndi9@gmail.com>
2023-10-06 10:38:09 +0200

    fix scheduler

--- app/routers/scrapers_schedulers.py
+++ app/routers/scrapers_schedulers.py
@@ -3,13 +3,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job("interval", hours=1)
+@scheduler.scheduled_job("interval", minutes=5)
 def sportnews_scrape():
     requests.get(
         url="http://localhost:8080/sportnews/scrape",
     )
 
-@scheduler.scheduled_job("interval", hours=1)
+@scheduler.scheduled_job("interval", seconds=30)
 def bets_scrape():
     requests.get(
         url="http://localhost:8080/bets/scrapebets"
