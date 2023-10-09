8aa6ff1940bbaaf0efcf50568b8ab2f1d6b222e3
IsaDuraku <isaduraku3@gmail.com>
2023-10-09 15:46:17 +0200

    Scheduler, backup scraper and updated router for livestream links.

--- app/routers/scrapers_schedulers.py
+++ app/routers/scrapers_schedulers.py
@@ -3,14 +3,23 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 scheduler = BackgroundScheduler()
 
+
 @scheduler.scheduled_job("interval", minutes=5)
 def sportnews_scrape():
     requests.get(
         url="http://localhost:8080/sportnews/scrape",
     )
 
+
 @scheduler.scheduled_job("interval", seconds=30)
 def bets_scrape():
     requests.get(
         url="http://localhost:8080/bets/scrapebets"
+    )
+
+
+@scheduler.scheduled_job("interval", hour=6)
+def link_scrape():
+    requests.get(
+        url="http://localhost:8080//livestream_scraper/scrape-and-insert"
     )
