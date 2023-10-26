5b90aeeac9ca5e5d36b70fc8d2f0f502d2633f43
Megzon Zejnullahu <megzonzej@gmail.com>
2023-10-26 15:38:35 +0000

    Update media.py

--- app/routers/media/media.py
+++ app/routers/media/media.py
@@ -5,7 +5,6 @@ from app.scrapers.media.media import get_media_links_with_text, insert_media_dat
 from app.database import SessionLocal
 from fastapi import HTTPException
 
-
 router = APIRouter(
     prefix='/media',
     tags=['media']
