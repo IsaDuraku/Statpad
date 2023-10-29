b6836fb68b7026d48288bb32179146ad7f0f2263
Temal Haliti <temal.haliti@makerminds.org>
2023-10-29 21:14:54 +0100

    Added favorite team logic in myteam

--- app/routers/myteam/myteam.py
+++ app/routers/myteam/myteam.py
@@ -19,20 +19,6 @@ router = APIRouter(
 )
 templates=Jinja2Templates(directory='templates')
 
-# @router.get("/favoriteteam")
-# def get_favorite_team(request: Request, user: UserDB = Depends(get_current_user), db: SessionLocal = Depends(get_db)):
-#     favorite_team = user.favorite_team
-#
-#     if not favorite_team:
-#         raise HTTPException(status_code=400, detail="User has not set a favorite team")
-#
-#     lineup = db.query(Lineup).filter(Lineup.team.ilike(f"%{favorite_team}%")).all()
-#
-#
-#     lineup_list = [LineupModel(**item.__dict__) for item in lineup]
-#
-#     return templates.TemplateResponse("myteam.html", {"request": request, "favorite_team": favorite_team,'lineup':lineup_list})
-
 @router.get('/favoriteteam')
 def get_favorite_team(request: Request):
     db = SessionLocal()
